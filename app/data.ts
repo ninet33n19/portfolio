6a0bc5a6904527ca4daa1e2f6b7edc04f8c1a089
ninet33n19 <vaidyameet2004@gmail.com>
2025-06-26 13:33:48 +0530

    initial commit

---
+++ app/data.ts
@@ -0,0 +1,66 @@
+type Project = {
+  name: string
+  description: string
+  link: string
+  video: string
+  id: string
+}
+
+type WorkExperience = {
+  company: string
+  title: string
+  start: string
+  end: string
+  link: string
+  id: string
+}
+
+type BlogPost = {
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
+type SocialLink = {
+  label: string
+  link: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Motion Primitives Pro',
+    description:
+      'Advanced components and templates to craft beautiful websites.',
+    link: 'https://pro.motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    id: 'project1',
+  },
+]
+
+export const WORK_EXPERIENCE: WorkExperience[] = [
+  {
+    company: 'Vighnotech',
+    title: 'React Developer Intern',
+    start: 'Jan 2025',
+    end: 'April 2025',
+    link: 'https://www.vighnotech.com/',
+    id: 'work1',
+  },
+]
+
+export const BLOG_POSTS: BlogPost[] = []
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    label: 'Github',
+    link: 'https://github.com/ninet33n19',
+  },
+  {
+    label: 'Twitter',
+    link: 'https://x.com/_ninet33n_',
+  },
+]
+
+export const EMAIL = 'vaidyameet2004@email.com'
